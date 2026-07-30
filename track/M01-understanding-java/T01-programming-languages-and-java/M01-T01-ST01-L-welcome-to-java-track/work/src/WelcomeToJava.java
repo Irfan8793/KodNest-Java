a19a285aa21b0bc6074d8fb1209deefa213bf2d3
Irfan Khan <irfanpathan3524@gmail.com>
2026-07-30 13:00:49 +0530

    added one more line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -3,6 +3,7 @@ public class WelcomeToJava {
         // Write your code here
         System.out.println("Welcome to the Java Track");
         System.out.println("I already know how to build programming logic");
-        System.out.println("Now I am learning how to express that logic using Java");
+        System.out.println("Now I am learning how to express that logic using Java.");
+        System.out.println("Now I know how to use git and github");
     }
 }
